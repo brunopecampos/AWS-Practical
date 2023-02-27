8264d48826f06d6f1f50dd9cd137bdf286c2004c
brunopecampos <brunopecampos@protonmail.com>
2023-02-27 10:41:58 -0300

    Add networking yaml of project
    Signed-off-by: brunopecampos <brunopecampos@protonmail.com>

--- EC2/user-data.sh
+++ EC2/user-data.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-yum update -y
-yum 
+# install git, node and yarn
+apt update -y
+curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash -
+sudo apt-get install -y nodejs git
+# For testing reasons
+#sudo apt-get install -y mysql-client
+sudo npm install --global yarn
+# clone backend subdir of repo
+cd /home/ubuntu
+git clone --depth 1 --filter=blob:none https://github.com/brunopecampos/AWS-Practical.git --sparse 
+cd AWS-Practical 
+git config --global --add safe.directory $PWD
+git sparse-checkout init --cone
+git sparse-checkout set ./EC2/backend
+# install dependencies and start server
+cd ./EC2/backend
+yarn install
+#node server.js
