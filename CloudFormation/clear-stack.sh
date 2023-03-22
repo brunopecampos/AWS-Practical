8f96326f42dcab9a68968214616ccef8e9f02e60
brunopecampos <brunopecampos@protonmail.com>
2023-03-22 11:16:02 -0300

    Added cloudformation to stack
    Signed-off-by: brunopecampos <brunopecampos@protonmail.com>

--- CloudFormation/clear-stack.sh
+++ CloudFormation/clear-stack.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+aws s3 rm s3://opus-bruno-campos-frontend-website --recursive
 aws cloudformation delete-stack --stack-name full-stack
 aws rds wait db-instance-deleted --db-instance-identifier opus-database
 aws cloudformation delete-stack --stack-name full-stack
+echo "Deleted full stack."
 aws s3 rm s3://opus-bruno-campos-stacks --recursive
+echo "Frontend bucket empty."
 aws cloudformation delete-stack --stack-name stack-bucket
--- CloudFormation/create-stack.sh
+++ CloudFormation/create-stack.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 aws cloudformation create-stack --stack-name stack-bucket --template-body file://stack-bucket.yaml 1>/dev/null
 aws cloudformation wait stack-create-complete --stack-name stack-bucket
-echo "Sucessefully created stack."
+echo "Successfully created stack bucket."
 aws s3 sync stack-bucket/ s3://opus-bruno-campos-stacks
+aws s3 cp ../EC2/cloudwatch-agent.json s3://opus-bruno-campos-stacks
 echo "Synchronized stack."
 aws s3api wait object-exists --bucket opus-bruno-campos-stacks --key vpc.yaml
-aws cloudformation create-stack --stack-name full-stack --template-body file://full-stack.yaml 1>/dev/null
+aws cloudformation create-stack p--capabilities CAPABILITY_NAMED_IAM --stack-name full-stack --template-body file://full-stack.yaml 1>/dev/null
 aws cloudformation wait stack-create-complete --stack-name full-stack
-#echo $(aws elbv2 describe-load-balancers --names opus-alb --query "LoadBalancers[0].DNSName" --output text) > ../S3/frontend/lb_endpoint
-
+echo "Successfully created main stack."
+cd ../S3/frontend
+REACT_APP_ENDPOINT=$(aws elbv2 describe-load-balancers --names opus-alb --query "LoadBalancers[0].DNSName" --output text) yarn build
+yarn deploy
+echo "Deployed frontend to S3 bucket"
